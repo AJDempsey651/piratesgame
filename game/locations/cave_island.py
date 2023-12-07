144bb265c99114b5699452abdaebb7cec26828b6
Adam Dempsey <dempseya651@gmail.com>
2023-12-07 11:43:01 -0500

    Update to pirates code #2

--- game/locations/cave_island.py
+++ game/locations/cave_island.py
@@ -3,6 +3,7 @@ from game.display import announce
 import game.config as config
 import game.items as item
 from game.events import *
+import random
 
 
 class CaveIsland(location.Location):
@@ -71,7 +72,6 @@ class Beach(location.SubLocation):
                     announce("You take the " + i.name + " out of the sand.")
                     config.the_player.add_to_inventory(i)
                     self.item_buried = None
-                    #this command uses time
                     config.the_player.go = True
                     at_least_one = True
                 i = self.item_buried_2
@@ -79,12 +79,10 @@ class Beach(location.SubLocation):
                     announce("You take the " + i.name + " out of the sand.")
                     config.the_player.add_to_inventory(i)
                     self.item_buried_2 = None
-                    # this command uses time
                     config.the_player.go = True
                     at_least_one = True
             if not at_least_one:
                 announce("You don't see one of those around.")
-
 
 
 class Forest(location.SubLocation):
@@ -134,9 +132,42 @@ class NorthCave(location.SubLocation):
         if verb == "east" or verb == "west":
             announce("You can't go this direction from here.")
         if verb == "north":
-            announce("You walked all the way to the end of the cave. There is nothing else here.")
+            config.the_player.next_loc = self.main_location.locations["north-cave-deeper"]
         if verb == "south":
             config.the_player.next_loc = self.main_location.locations["forest"]
+
+
+class NorthCaveDeeper(location.SubLocation):
+    def __init__(self, main_location):
+        super().__init__(main_location)
+        self.name = "north-cave-deeper"
+        self.verbs["north"] = self
+        self.verbs["south"] = self
+        self.verbs["east"] = self
+        self.verbs["west"] = self
+        self.verbs["take"] = self
+        self.treasurechest = TreasureChest()
+
+    def enter(self):
+        if self.treasurechest is None:
+            announce("You have gone deeper into the cave but there is nothing else here.")
+        elif self.treasurechest == TreasureChest():
+            announce("You have gone deeper into the cave. It's dark but you can make out the shape of a Treasure Chest!")
+
+    def process_verbs(self, verb, cmd_list, nouns):
+        if verb == "east" or verb == "west":
+            announce("You can't go this direction from here.")
+        if verb == "north":
+            announce("You cannot go deeper into this cave.")
+        if verb == "south":
+            config.the_player.next_loc = self.main_location.locations["north-cave"]
+        if verb == "take":
+            if self.treasurechest is None:
+                announce("You don't see anything to take.")
+            if self.treasurechest == TreasureChest():
+                announce("You found treasure!")
+                config.the_player.add_to_inventory(TreasureChest())
+                self.treasurechest = None
 
 
 class WestCave(location.SubLocation):
@@ -157,7 +188,42 @@ class WestCave(location.SubLocation):
         if verb == "east":
             config.the_player.next_loc = self.main_location.locations["forest"]
         if verb == "west":
-            announce("You have walked all the way to the end of the cave. There is nothing else in there.")
+            config.the_player.next.loc = self.main_location.locations["west-cave-deeper"]
+
+
+class WestCaveDeeper(location.SubLocation):
+    def __init__(self, main_location):
+        super().__init__(main_location)
+        self.name = "west-cave-deeper"
+        self.verbs["north"] = self
+        self.verbs["south"] = self
+        self.verbs["east"] = self
+        self.verbs["west"] = self
+        self.treasure = SmallTreasure()
+
+    def enter(self):
+        if self.treasure == SmallTreasure():
+            announce("You have gone deeper into the cave. There is some treasure on the ground and "
+                     "it looks like there is a message on the wall above. It says... I am east of here, "
+                     "but south of south, I am hidden in the dark even when it's day.")
+        elif self.treasure is None:
+            announce("You have gone deeper into the cave. The message on the wall still says...I am east of here, "
+                     "but south of south, I am hidden in the dark even when it's day. There is nothing else here.")
+
+    def process_verbs(self, verb, cmd_list, nouns):
+        if verb == "north" or verb == "south":
+            announce("You can't go that direction from here.")
+        if verb == "east":
+            config.the_player.next_loc = self.main_location.locations["west-cave"]
+        if verb == "west":
+            announce("You cannot go deeper into this cave. It is a dead end.")
+        if verb == "take":
+            if self.treasure is None:
+                announce("You don't see anything to take.")
+            if self.treasure == SmallTreasure():
+                announce("You found treasure!")
+                config.the_player.add_to_inventory(SmallTreasure())
+                self.treasure = None
 
 
 class Lagoon(location.SubLocation):
@@ -170,8 +236,29 @@ class Lagoon(location.SubLocation):
         self.verbs["west"] = self
         self.verbs["take"] = self
         self.item_floating = item.Cutlass()
-        self.event_chance = 50
-        self.events.append(drowned_pirates.DrownedPirates())
+        self.treasure = Treasure()
+
+    def monster_attack(self, combat):
+        result = {}
+        result["message"] = "the drowned pirates are defeated!"
+        monsters = []
+        min = 2
+        uplim = 6
+        if random.randrange(2) == 0:
+            min = 1
+            uplim = 5
+            monsters.append(combat.Drowned("Pirate captain"))
+            monsters[0].speed = 1.2 * monsters[0].speed
+            monsters[0].health = 2 * monsters[0].health
+        n_appearing = random.randrange(min, uplim)
+        n = 1
+        while n <= n_appearing:
+            monsters.append(combat.Drowned("Drowned pirate " + str(n)))
+            n += 1
+        announce("You are attacked by a crew of drowned pirates!")
+        combat.Combat(monsters).combat()
+        result["newevents"] = [self]
+        return result
 
     def enter(self):
         description = ("You have arrived at a lagoon in the forest on the south end of the island.")
@@ -196,3 +283,20 @@ class Lagoon(location.SubLocation):
                 config.the_player.add_to_inventory(item.Cutlass())
                 self.item_floating = None
 
+
+class TreasureChest(item.Item):
+    def __init__(self):
+        super().__init__("treasurechest", 500)
+        self.value = 500
+
+
+class Treasure(item.Item):
+    def __init__(self):
+        super().__init__("treasure", 100)
+        self.value = 100
+
+
+class SmallTreasure(item.Item):
+    def __init__(self):
+        super().__init__("smalltreasure", 50)
+        self.value = 50
